f6ca9667ae77511c40f88d2c28fcc8f63ea62b65
Mikhail Scherba <miklezzzz@mail.ru>
2018-12-09 15:04:43 +0500

    GitlabCI installation and configuration

---
+++ gitlab-ci/terraform/variables.tf
@@ -0,0 +1,36 @@
+variable app_disk_image {
+  description = "Disk image for reddit app"
+  default     = "reddit-app-base"
+}
+
+variable db_disk_image {
+  description = "Disk image for reddit db"
+  default     = "reddit-db-base"
+}
+
+variable project {
+  description = "Project ID"
+}
+
+variable region {
+  description = "Region"
+  default     = "europe-west1"
+}
+
+variable zone {
+  description = "Zone"
+  default     = "europe-west1-b"
+}
+
+variable public_key_path {
+  description = "Path to the public key for ssh"
+}
+
+variable disk_image {
+  description = "Diks image"
+}
+
+variable ssh_key_for_provisioners {
+  description = "SSH private key for provisioners"
+}
+
