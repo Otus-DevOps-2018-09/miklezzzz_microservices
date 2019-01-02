4cd6f01b1c505c0c94476a4c6d6d1c37c0439dff
Mikhail Scherba <miklezzzz@mail.ru>
2019-01-02 17:15:13 +0500

    gitlabCI pipelines added with builde&deploy steps

--- docker-monolith/start.sh
+++ docker-monolith/start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-/usr/bin/mongod --fork --logpath /var/log/mongod.log --config /etc/mongodb.conf
+/usr/bin/mongod --fork --logpath /var/log/mongod.log --config /etc/mongod.conf
 source /reddit/db_config
 cd /reddit && puma || exit
 
