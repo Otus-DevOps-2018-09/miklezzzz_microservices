d15bc11853c80aed53eee0dc613ed427ea979040
Mikhail Scherba <miklezzzz@mail.ru>
2019-01-10 22:02:45 +0500

    Conditionals added to the alertmanager entrypoint

--- monitoring/alertmanager/entrypoint.sh
+++ monitoring/alertmanager/entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-sed -i "s/SMTP_TO/$SMTP_TO/" /etc/alertmanager/config.yml
-sed -i "s/SMTP_SMARTHOST/$SMTP_SMARTHOST/" /etc/alertmanager/config.yml
-sed -i "s/SMTP_AUTH_USERNAME/$SMTP_AUTH_USERNAME/g" /etc/alertmanager/config.yml
-sed -i "s/SMTP_AUTH_PASSWORD/$SMTP_AUTH_PASSWORD/g" /etc/alertmanager/config.yml
+if [ "$SMTP_TO" != "" ]; then sed -i "s/SMTP_TO/$SMTP_TO/" /etc/alertmanager/config.yml; fi
+if [ "$SMTP_SMARTHOST" != "" ]; then sed -i "s/SMTP_SMARTHOST/$SMTP_SMARTHOST/" /etc/alertmanager/config.yml; fi
+if [ "$SMTP_AUTH_USERNAME" != "" ]; then sed -i "s/SMTP_AUTH_USERNAME/$SMTP_AUTH_USERNAME/g" /etc/alertmanager/config.yml; fi
+if [ "$SMTP_AUTH_PASSWORD" != "" ]; then sed -i "s/SMTP_AUTH_PASSWORD/$SMTP_AUTH_PASSWORD/g" /etc/alertmanager/config.yml; fi
 
 /bin/alertmanager $@
