935adacf8b3255d24f76711c0f8baa36356ec0fa
Mikhail Scherba <miklezzzz@mail.ru>
2019-01-10 22:23:36 +0500

    Alertmanager config renamed to alertmanager.yml

--- monitoring/alertmanager/entrypoint.sh
+++ monitoring/alertmanager/entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-if [ "$SMTP_TO" != "" ]; then sed -i "s/SMTP_TO/$SMTP_TO/" /etc/alertmanager/config.yml; fi
-if [ "$SMTP_SMARTHOST" != "" ]; then sed -i "s/SMTP_SMARTHOST/$SMTP_SMARTHOST/" /etc/alertmanager/config.yml; fi
-if [ "$SMTP_AUTH_USERNAME" != "" ]; then sed -i "s/SMTP_AUTH_USERNAME/$SMTP_AUTH_USERNAME/g" /etc/alertmanager/config.yml; fi
-if [ "$SMTP_AUTH_PASSWORD" != "" ]; then sed -i "s/SMTP_AUTH_PASSWORD/$SMTP_AUTH_PASSWORD/g" /etc/alertmanager/config.yml; fi
+if [ "$SMTP_TO" != "" ]; then sed -i "s/SMTP_TO/$SMTP_TO/" /etc/alertmanager/alertmanager.yml; fi
+if [ "$SMTP_SMARTHOST" != "" ]; then sed -i "s/SMTP_SMARTHOST/$SMTP_SMARTHOST/" /etc/alertmanager/alertmanager.yml; fi
+if [ "$SMTP_AUTH_USERNAME" != "" ]; then sed -i "s/SMTP_AUTH_USERNAME/$SMTP_AUTH_USERNAME/g" /etc/alertmanager/alertmanager.yml; fi
+if [ "$SMTP_AUTH_PASSWORD" != "" ]; then sed -i "s/SMTP_AUTH_PASSWORD/$SMTP_AUTH_PASSWORD/g" /etc/alertmanager/alertmanager.yml; fi
 
 /bin/alertmanager $@
